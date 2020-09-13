03fd698c8e12b9df1fe630fb9782e8d1f7d71fdb
root <raymondkalonji@gmail.com>
2020-09-13 14:03:23 +0000

    docs: updating documentation

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,49 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+        Description:
+            Drop all tables
+
+        Parameters:
+            cur (cursor) : database cursor
+            conn (connection) : connection to a database
+
+        Returns:
+    """
+    
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+        Description:
+            Create all tables
+
+        Parameters:
+            cur (cursor) : database cursor
+            conn (connection) : connection to a database
+
+        Returns:
+    """
+    
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+        Description:
+            Load configuration parameters, establishes database connection,
+            drop all tables and then create them
+
+        Parameters:
+
+        Returns:
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,18 +4,50 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Description:
+        Load S3 data into into the staging tables
+    
+    Parameters:
+        cur (cursor) : database cursor
+        conn (connection) : connection to a database
+    
+    Returns:
+    """
+        
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+        Description:
+            Reads data from the staging tables and insert it
+            into the final tables
+
+        Parameters:
+            cur (cursor) : database cursor
+            conn (connection) : connection to a database
+
+        Returns:
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+        Description:
+            Load configuration parameters, establishes database connection
+            and call function performing ETL tasks
+
+        Parameters:
+
+        Returns:
+    """
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
